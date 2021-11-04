8671be3286d655a5b361d7b32fa87f8fcaeffa75
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-11-04 19:53:07 -0400

    fitted the landing page image in place, learned object-fit/position

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import sortingVizImage from '../Images/sortingVizImage.jpg';
 
 export default function LandingPage() {
   return (
-    <div className="page-container">
+    <div className="lp-page-container">
       <div className="lp-subcontainer lp-left"></div>
-      <div className="lp-subcontainer lp-right"></div>
+      <div className="lp-subcontainer lp-right">
+        <div className="image-container">
+          <img className="lp-image" src={sortingVizImage} alt="logo" />
+        </div>
+      </div>
     </div>
   );
 }
