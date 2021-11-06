6f8c05723ce24c8e4ff0885cb6c95742b359423d
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-11-06 11:09:31 -0400

    Setting up Sorting Page, learning more CSS

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -3,12 +3,8 @@ import React from 'react';
 export default function Controls() {
   return (
     <div className="controls-container">
-      <span className="controls-text"></span>
-      <div className="button-container"></div>
-      <div className="controls-bottom">
-        <div className="sliders"></div>
-        <div className="start-button"></div>
-      </div>
+      <div className="algo-buttons-container"></div>
+      <div className="sliders"></div>
     </div>
   );
 }
