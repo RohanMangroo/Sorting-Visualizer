9bc70350c4f6cd41ecd72b58b60de1b6350c1505
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-11-05 23:52:29 -0400

    final commit for the night

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default function Controls() {
-  return <div className="controls"></div>;
+  return (
+    <div className="controls-container">
+      <span className="controls-text"></span>
+      <div className="button-container"></div>
+      <div className="controls-bottom">
+        <div className="sliders"></div>
+        <div className="start-button"></div>
+      </div>
+    </div>
+  );
 }
