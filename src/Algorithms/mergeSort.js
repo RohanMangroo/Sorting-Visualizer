fefb8a00f929d9ea18f044d3b54bc1deeec70845
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-11-19 01:37:58 -0500

    commented mergeSort

--- src/Algorithms/mergeSort.js
+++ src/Algorithms/mergeSort.js
@@ -1,29 +1,25 @@
 import { colorBars, colors } from '../utils';
 import { displaySort } from '../utils';
 
-//================================================================================================================//
+/**====================================================================================================================================
+ * The array of numbers being sorted and the array of bars on the screen are NOT one in the same, but they do have a connection
+ * When the array of numbers is generated, an array of heights is also generated
+ * The array of heights is based off of the random numbers AND the current height of the container
+ * This way we can assign the height of the bar as a percentage, so now the bar height will always be responsive to the container height
+ * So no matter the current heights of the bars on the screen they always maintain their relationship to the numbers that generated them
+=======================================================================================================================================*/
 
-export default async function mergeSort(
-  arr,
-  bars,
-  speed,
-  heights,
-  setButton,
-  updateSwaps_,
-  updateChecks_,
-  updateRecursiveSplits_
-) {
-  const metricsInfo = {
-    updateChecks_,
-    updateSwaps_,
-    updateRecursiveSplits_,
-    checks: 0,
-    swaps: 0,
-    splits: 0,
-  };
-  if (arr.length <= 1) return;
+export default async function mergeSort(bars, updates) {
+  if (bars.nums.length <= 1) return;
 
-  bars = Array.from(bars);
+  //The bars grabbed from the DOM are 'array-like' and not an actual array so we need to convert it to an array by calling Array.from()
+  bars.displayBars = Array.from(bars.displayBars);
+
+  //Destructuring the Redux states
+  const { nums: arr, initialHeights: heights } = bars;
+
+  //Here we are creating an object that holds all the metric info that we will pass down
+  const metricsInfo = { swaps: 0, checks: 0, splits: 0, updates };
 
   const auxArr = arr.slice();
   const auxHeights = heights.slice();
@@ -36,128 +32,95 @@ export default async function mergeSort(
     heights,
     auxHeights,
     bars,
-    speed,
     metricsInfo
   );
-  // for (let i = 0; i < arr.length; i++) {
-  //   if (i < arr.length) {
-  //     await colorBars([i], colors.sorted, bars, speed, bars);
-  //   }
-  // }
+  updates.updateMainButton_('NEW');
+}
+/*================================================================================================================*/
 
-  setButton({
-    btnName: 'NEW',
-    btnType: 'new-array-btn',
-  });
+/*
+I decided to shorten the names for the prameters because there were to many and the formatting was looking un-readable
+mergeHelper(start, stop, main, auxArray, heights, auxHeights, bars, metricsInfo)
+*/
+
+async function mergeHelper(st, sp, mn, ax, hts, axHts, bars, metInfo) {
+  const { updateRecursiveSplits_ } = metInfo.updates;
+  const { displayBars: dP, speed: spd } = bars;
+
+  //Base case
+  if (st === sp) return;
+  //Calculating the midPosition, this is what we will use to determine the left and right splits
+  const mP = Math.floor((st + sp) / 2);
+  //Updating the recursive split count
+  updateRecursiveSplits_(++metInfo.splits);
+
+  //It's important to know that in this version of merge sort the main and aux arrays will filp positions each time they are passed in to the recursive mergeHelper function
+
+  //Here we are recursivly splitting left
+  await mergeHelper(st, mP, ax, mn, axHts, hts, bars, metInfo);
+  //Here we are recursivly splitting right
+  await mergeHelper(mP + 1, sp, ax, mn, axHts, hts, bars, metInfo);
+
+  //Here we are merging our two halves. We pass in st(start), sp(stop) and mP(nidPoint) that way we can have access to the two halves
+  await merge(st, sp, mP, mn, ax, hts, axHts, bars, metInfo);
+
+  //Here we are cloring the bars
+  await displaySort(st, sp, dP, hts, spd, colors);
 }
 
-//================================================================================================================//
+//mp === midPosition
+async function merge(st, sp, mP, mn, ax, hts, axHts, bars, metInfo) {
+  const { updateChecks_ } = metInfo.updates;
+  const { speed, displayBars } = bars;
 
-async function mergeHelper(
-  srt,
-  stp,
-  main,
-  aux,
-  hgts,
-  auxHgts,
-  bars,
-  spd,
-  metricsInfo
-) {
-  if (srt === stp) return;
-  const midPoint = Math.floor((srt + stp) / 2);
-  metricsInfo.updateRecursiveSplits_(++metricsInfo.splits);
-  await mergeHelper(
-    srt,
-    midPoint,
-    aux,
-    main,
-    auxHgts,
-    hgts,
-    bars,
-    spd,
-    metricsInfo
-  );
-  await mergeHelper(
-    midPoint + 1,
-    stp,
-    aux,
-    main,
-    auxHgts,
-    hgts,
-    bars,
-    spd,
-    metricsInfo
-  );
-  await merge(
-    srt,
-    stp,
-    midPoint,
-    main,
-    aux,
-    hgts,
-    auxHgts,
-    bars,
-    spd,
-    metricsInfo
-  );
-  await displaySort(srt, stp, bars, hgts, spd, colors);
-}
-
-async function merge(
-  start,
-  stop,
-  midPoint,
-  main,
-  aux,
-  heights,
-  auxHeights,
-  bars,
-  speed,
-  metricsInfo
-) {
   //'k' is the position in the main array
-  let k = start;
+  let k = st;
   //'i' is the beginning of the first half of the aux array
-  let i = start;
+  let i = st;
   //'j' is the beginning of the second half of the aux array
-  let j = midPoint + 1;
+  let j = mP + 1;
 
-  //midPoint is the end of the first half
-  //stop is the end of the second half
+  //mP(midPoint) is the end of the first half
+  //sp(stop) is the end of the second half
 
-  while (i <= midPoint && j <= stop) {
-    metricsInfo.updateChecks_(++metricsInfo.checks);
-    await colorBars([k], colors.red, bars, speed);
-    if (aux[i] <= aux[j]) {
-      main[k] = aux[i];
-      heights[k] = auxHeights[i];
+  while (i <= mP && j <= sp) {
+    //Updating the checks count
+    updateChecks_(++metInfo.checks);
+    await colorBars([k], colors.red, displayBars, speed);
+    //Here we have two halves that we are comparing
+    //We will establish two pointers(i, j) for each respective half
+    //The lesser of the two values will be placed in the main array at the k position
+    if (ax[i] <= ax[j]) {
+      mn[k] = ax[i];
+      hts[k] = axHts[i];
       i++;
     } else {
-      main[k] = aux[j];
-      heights[k] = auxHeights[j];
+      mn[k] = ax[j];
+      hts[k] = axHts[j];
       j++;
     }
-    colorBars([k], 'white', bars);
+    colorBars([k], colors.sorted, displayBars);
     k++;
   }
 
-  while (i <= midPoint) {
-    metricsInfo.updateChecks_(++metricsInfo.checks);
-    await colorBars([k], colors.red, bars, speed);
-    main[k] = aux[i];
-    heights[k] = auxHeights[i];
-    colorBars([k], 'white', bars, speed);
+  //This while loop will handle if position 'i' is still in bounds
+  while (i <= mP) {
+    updateChecks_(++metInfo.checks);
+    await colorBars([k], colors.red, displayBars, speed);
+    mn[k] = ax[i];
+    hts[k] = axHts[i];
+    colorBars([k], colors.sorted, displayBars, speed);
     i++;
     k++;
   }
 
-  while (j <= stop) {
-    metricsInfo.updateChecks_(++metricsInfo.checks);
-    await colorBars([k], colors.red, bars, speed);
-    main[k] = aux[j];
-    heights[k] = auxHeights[j];
-    colorBars([k], 'white', bars, speed);
+  //This while loop will handle if position 'j' is still in bounds
+  while (j <= sp) {
+    updateChecks_(++metInfo.checks);
+    await colorBars([k], colors.red, displayBars, speed);
+    mn[k] = ax[j];
+    hts[k] = axHts[j];
+    colorBars([k], colors.sorted, displayBars, speed);
     j++;
     k++;
   }
--- src/Algorithms/quickSort.js
+++ src/Algorithms/quickSort.js
@@ -43,20 +43,28 @@ async function quickSortHelper(start, stop, bars, metricsInfo) {
     }
     return array;
   }
-
+  //Establishing our positions
   let pivot = start;
+  //Here we will color the current pivot green
   colorBars([pivot], colors.green, displayBars);
   let left = start + 1;
   let right = stop;
 
   while (left <= right) {
+    //Our compared bars(left and right) will be colored red
     await colorBars([left, right], colors.red, displayBars, speed);
+
     if (array[left] > array[pivot] && array[right] < array[pivot]) {
+      //If those compared bars need to be swapped we will color them green
       await colorBars([left, right], colors.green, displayBars, speed);
+      //Here we are swapping the actual numbers in the array
       swapNums(left, right, array);
+      //Here we are swapping bar heights on the screen
       swapBarHeights(left, right, displayBars);
+      //Hee we are updating swaps count
       updateSwaps_(++metricsInfo.swaps);
     } else {
+      //If the value at array[left] is less than the pivot that means it's not what we are looking for so turn that bar white and move on
       if (array[left] <= array[pivot]) {
         updateChecks_(++metricsInfo.checks);
         colorBars([left], 'white', displayBars);
@@ -66,6 +74,7 @@ async function quickSortHelper(start, stop, bars, metricsInfo) {
           await colorBars([left], colors.red, displayBars, speed);
         }
       }
+      //If the value at array[right] is greater than the pivot that means it's not what we are looking for so turn that bar white and move on
       if (array[right] >= array[pivot]) {
         updateChecks_(metricsInfo.checks);
         colorBars([right], 'white', displayBars);
@@ -77,20 +86,33 @@ async function quickSortHelper(start, stop, bars, metricsInfo) {
     }
   }
 
-  await colorBars([pivot, right], colors.green, displayBars, speed);
+  //After the two left and right pointer have eclipsed, the pivot number and right will swap positions
+
+  //Here we coloring them both green
+  colorBars([pivot, right], colors.green, displayBars, speed);
+  //Here we are swapping the numbers in the actual array
   swapNums(pivot, right, array);
+  //Here we are swapping the bar heights on the screen
   swapBarHeights(pivot, right, displayBars);
+  //Here I'm using the 'sorted' color to colore them(in reality only the just swapped pivot is in sorted order)
   colorBars([right], colors.sorted, displayBars);
+  //Here i'm updating the swap count
+  updateSwaps_(++metricsInfo.swaps);
 
   if (left <= array.length - 1) {
     colorBars([left], 'white', displayBars);
   }
-
+  //Here we are coloring the pivot position(which was just swapped) white
   colorBars([pivot], 'white', displayBars);
+
+  //Here we are updating the recursive split count
   updateRecursiveSplits_(++metricsInfo.splits);
+
+  //The next 2 lines are the recursion needed for quicksort to work. At every recursive split we pass in different parameters that act as new boundries to an array that quicksort will operate on
   await quickSortHelper(start, right - 1, bars, metricsInfo);
   await quickSortHelper(right + 1, stop, bars, metricsInfo);
 
+  //Here we are coloring all the bars that are done with their respective recursive splitting, sorted
   for (let i = start; i <= stop + 1; i++) {
     if (i < array.length - 1) {
       colorBars([i], colors.sorted, displayBars);
